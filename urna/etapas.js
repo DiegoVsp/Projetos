c2147bbff6b252c1da5ffa5aa05e01910f891f2a
Diego Vespa <diego-vespa2020@outlook.com.br>
2020-09-26 11:15:40 -0300

    atualizando projeto

---
+++ urna/etapas.js
@@ -0,0 +1,56 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '38111',
+        nome: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos: [
+          { url: 'dog.jpg', legenda: 'Vereador' }
+        ]
+      }
+    ]
+  },
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '39111',
+        nome: 'Jurema de Tal',
+        partido: 'CBA',
+        fotos: [
+          { url: 'cv1.jpg', legenda: 'Vereador' }
+        ]
+      }
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        nome: 'Ciclano',
+        partido: 'ABC',
+        vice: 'Zac',
+        fotos: [
+          {url:'cv1.jpg', legenda: 'Prefeito'},
+          {url:'cv2.jpg', legenda: 'Vice-Prefeito', small:true},
+        ]
+      },
+      {
+        numero: '84',
+        nome: 'Zulano',
+        partido: 'CBA',
+        vice: 'Cic',
+        fotos: [
+          {url:'cv3.jpeg', legenda: 'Prefeito'},
+          {url:'cv4.jpg', legenda: 'Vice-Prefeito', small:true},
+        ]
+      }
+    ]
+  }
+]
---
+++ urna/script.js
@@ -0,0 +1,70 @@
+let seuVotoPara = document.querySelector('.d-1-1 span')
+let cargo = document.querySelector('.d-1-2 span')
+let descricao = document.querySelector('.d-1-4')
+let aviso = document.querySelector('.d-2')
+// aviso.style.display='none'
+let lateral = document.querySelector('.d-1-right')
+let numeros = document.querySelector('.d-1-3')
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+  let etapa = etapas[etapaAtual]
+
+  let numeroHTML = '';
+
+  for(let i = 0;i < etapa.numeros; i++) {
+    if(i===0) {
+      numeroHTML = '<div class="numero pisca"></div>'
+    }else {
+      numeroHTML += '<div class="numero"></div>'
+    }
+  }
+  seuVotoPara.style.display = 'none'
+  cargo.innerHTML = etapa.titulo
+  descricao.innerHTML = '';
+  aviso.style.display = 'none'
+  lateral.innerHTML = ''
+  numeros.innerHTML = numeroHTML
+}
+
+function atualizaInterface() {
+  let etapa = etapas[etapaAtual]
+  let candidato = etapa.candidatos.filter((item) => {
+    if(item.numero === numero) {
+      return true;
+    }else {
+      return false;
+    }
+  })
+  console.log('Candidato', candidato)
+  
+}
+
+function clicou(n) {
+  let elementoNumero = document.querySelector('.numero.pisca')
+  if(elementoNumero !== null) {
+    elementoNumero.innerHTML = n
+    numero = `${numero}${n}`;
+
+    elementoNumero.classList.remove('pisca')
+    if(elementoNumero.nextElementSibling !== null){
+      // adiciona no proximo elemento a classe pisca
+      elementoNumero.nextElementSibling.classList.add('pisca')
+    }else {
+      atualizaInterface();
+    }
+  }
+}
+function branco() {
+  alert('branco')
+}
+function corrige() {
+  alert('corrige')
+}
+function confirma() {
+  alert('confirma')
+}
+
+comecarEtapa()
